3735965d7938aa3381a38ef9d830136689d118fd
Peter Szabo <pts@fazekas.hu>
2001-06-09 22:52:56 +0100

    improved slow_pii.rb

--- slow_pii.rb
+++ slow_pii.rb
@@ -1,7 +1,11 @@
-(s=$>).sync=s<<'3.'
-k,a,b,f,g=3,0,4,40,24
+(s=$>).sync=s<<3.1
+#a,b,f,g,p,q=0,4,40,24,9,7
+#a,b,f,g,p,q=160,24,760,204,16,9
+a,b,f,g,p,q=40,6,190,104,16,9
 while a
-p,q,k=k*k,2*k+1,k+1
 a,b,f,g=f,g,p*a+q*f,p*b+q*g
-a,f,s=10*(a%b),10*(f%g),s<<p while f/g==p=a/b
+s,a,f=s<<$_,10*(a%b),10*(f%g)while f/g==$_=a/b
+p+=q
+q+=2
+#p a,b,f,g,p,q;exit
 end
