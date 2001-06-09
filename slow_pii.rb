3fdf09a501dd0f4943d33c753ba8df3a674ba410
Peter Szabo <pts@fazekas.hu>
2001-06-09 22:36:24 +0100

    improved slow_pii.rb

--- slow_pii.rb
+++ slow_pii.rb
@@ -1,14 +1,7 @@
-STDOUT.sync=!print("3.")
-k,a,b,f,g = 3,0,4,40,24
-
-while k
-  p,q,k = k*k,2*k+1,k+1
-  a,b,f,g = f,g,p*a+q*f,p*b+q*g
-  d = a / b
-  e = f / g
-  while d == e
-    print d
-    a,f = 10*(a%b),10*(f%g)
-    d,e = a/b,f/g
-  end
+(s=$>).sync=s<<'3.'
+k,a,b,f,g=3,0,4,40,24
+while a
+p,q,k=k*k,2*k+1,k+1
+a,b,f,g=f,g,p*a+q*f,p*b+q*g
+a,f,s=10*(a%b),10*(f%g),s<<p while f/g==p=a/b
 end
