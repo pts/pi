ebf01789a0e7cb7e89e2b4925ded4d9878bfb0a5
Peter Szabo <pts@fazekas.hu>
2001-06-10 13:58:39 +0100

    improved slow_pii.pl

--- slow_pii.pl
+++ slow_pii.pl
@@ -1,21 +1,27 @@
-#!/usr/local/bin/perl -w
+#!/usr/local/bin/perl
 use integer;
-use string;
+use strict;
+require'bigint.pl';
 
+# die "".bdiv("+76",+"24");
 
-k, a, b, f, g = 2, 4, 1, 12, 4
-
-while TRUE
+$|=1;
+print"3.";
+my($k,$a,$b,$f,$g,$p,$q,$d,$e,$l)=(3, 0, 4, 40, 24);
+while (1) {
   # Next approximation
-  p, q, k = k*k, 2*k+1, k+1
-  a, b, f, g = f, g, p*a+q*f, p*b+q*g
+  $l=badd($k,1);
+  ($p,$q,$k)=(bmul($k,$k),badd($k,$l),$l);
+  ($a,$b,$f,$g)=($f,$g,badd(bmul($p,$a),bmul($q,$f)),badd(bmul($p,$b),bmul($q,$g)));
+  # print "$a $b $f $g\n"; sleep 1;
   # Print common digits
-  d = a / b
-  e = f / g
-  while d == e
-    print d
-    $stdout.flush
-    a, f = 10*(a%b), 10*(f%g)
-    d, e = a/b, f/g
-  end
-end
+  while (1) {
+    ($d,$e)=(scalar(bdiv($a,$b)),bdiv($f,$g));
+    # die "".bdiv($f,$g);
+    # print "de $d $e $f $g\n";
+    last if $d ne$e;
+    print 0+$d;
+    # die;
+    ($a,$f)=(bmul(10,bmod($a,$b)),bmul(10,bmod($f,$g)));
+  }
+}
