1894884434829ace70d2e15bbcd7b896f9085703
Peter Szabo <pts@fazekas.hu>
2001-06-10 14:05:23 +0100

    improved slow_pii.pl

--- slow_pii.pl
+++ slow_pii.pl
@@ -4,13 +4,9 @@ use strict;
 require'bigint.pl';
 $|=1;
 print"3.";
-my($k,$a,$b,$f,$g,$p,$q,$d,$e,$l)=(3,0,4,40,24,9,5);
-while (1) {
-  $l=badd($k,1);
-  ($p,$q,$k)=(bmul($k,$k),badd($k,$l),$l);
+my($a,$b,$f,$g,$p,$q,$d,$e,$l)=(0,4,40,24,9,5);
+while ($q=badd($q,2)) {
   ($a,$b,$f,$g)=($f,$g,badd(bmul($p,$a),bmul($q,$f)),badd(bmul($p,$b),bmul($q,$g)));
-  while (bdiv($f,$g)eq($d=bdiv($a,$b))) {
-    print 0+$d;
-    ($a,$f)=(bmul(10,bmod($a,$b)),bmul(10,bmod($f,$g)));
-  }
+  ($a,$f)=(bmul(10,bmod($a,$b)),bmul(10,bmod($f,$g)),print$d+0)while($d=bdiv($a,$b))eq bdiv($f,$g);
+  $p=badd($p,$q);
 }
