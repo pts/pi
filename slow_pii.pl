33fa47b612518c89bc1b0f38881fc42e52b3a39d
Peter Szabo <pts@fazekas.hu>
2001-06-10 14:10:41 +0100

    improved slow_pii.pl

--- slow_pii.pl
+++ slow_pii.pl
@@ -1,12 +1,4 @@
-#!/usr/local/bin/perl
-use integer;
-use strict;
-require'bigint.pl';
-$|=1;
-print"3.";
-my($a,$b,$f,$g,$p,$q,$d,$e,$l)=(0,4,40,24,9,5);
-while ($q=badd($q,2)) {
-  ($a,$b,$f,$g)=($f,$g,badd(bmul($p,$a),bmul($q,$f)),badd(bmul($p,$b),bmul($q,$g)));
-  ($a,$f)=(bmul(10,bmod($a,$b)),bmul(10,bmod($f,$g)),print$d+0)while($d=bdiv($a,$b))eq bdiv($f,$g);
-  $p=badd($p,$q);
-}
+require'bigint.pl';$|=1;print"3.";($a,$b,$f,$g,$p,$q)=(0,4,40,24,9,5);while($q
+=badd($q,2)){($a,$b,$f,$g)=($f,$g,badd(bmul($p,$a),bmul($q,$f)),badd(bmul($p,$
+b),bmul($q,$g)));($a,$f)=(bmul(10,bmod($a,$b)),bmul(10,bmod($f,$g)),print$_+0)
+while($_=bdiv($a,$b))eq bdiv($f,$g);$p=badd($p,$q)}
