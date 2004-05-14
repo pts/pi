8c1c5aff07ca66b61c03ab8b401e8c276008ad03
Peter Szabo <pts@fazekas.hu>
2004-05-14 21:41:08 +0100

    improved slow_pii.py

--- slow_pii.py
+++ slow_pii.py
@@ -1,18 +1,9 @@
-#! /usr/bin/python2.4
-
 import sys
-
-k, a, b, a1, b1 = 2, 4, 1, 12, 4
-
-while True:
-  # Next approximation
-  p, q, k = k*k, 2*k+1, k+1
-  a, b, a1, b1 = a1, b1, p*a+q*a1, p*b+q*b1
-  # Print common digits
-  d = a / b
-  d1 = a1 / b1
-  while d == d1:
-    sys.stdout.write(str(d))
-    sys.stdout.flush()
-    a, a1 = 10*(a%b), 10*(a1%b1)
-    d, d1 = a/b, a1/b1
+w=sys.stdout.write
+a,b,f,g,p,q=0,4,40L,24,9,7L
+w('3.')
+while 1:
+ a,b,f,g=f,g,p*a+q*f,p*b+q*g
+ t=a/b
+ while f/g==t:w(repr(t)[-2]);sys.stdout.flush();a,f=10*(a%b),10*(f%g);t=a/b
+ p,q=p+q,q+2
