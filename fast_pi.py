6bbe0cf5fdab9779244213c80b9ffe3aac8b79e0
Peter Szabo <pts@fazekas.hu>
2010-07-17 15:45:02 +0100

    improved fast_pi.py

--- fast_pi.py
+++ fast_pi.py
@@ -2,7 +2,7 @@
 
 import sys
 
-def acot(x, u):
+def arcctg(x, u):
   sum = xpower = u // x
   xx = x * x
   n = 3
@@ -20,74 +20,46 @@ def acot(x, u):
     sum += term
     n += 2
 
-def maxerr(digits):
-  return (286135312 * digits + 31739381 + 9999999) // 10000000
+def pi0(digits0):
+  u = 10 * digits0
+  return 4 * (4*arcctg(5, u) - arcctg(239, u))
 
-def numdigits(v):
-  n = 1
-  while v > 9:
-    a = b = 1
-    pa = pb = 10
-    while v > pb:
-      a = b
-      pa = pb
-      pb *= pb
-      b *= 2
-    v //= pa
-    n += a
-  return n
+def maxerr(digits0):
+  return (286135312 * digits0 + 31739381 + 9999999) // 10000000
 
-assert numdigits(0) == 1
-assert numdigits(9) == 1
-assert numdigits(10) == 2
-assert numdigits(11) == 2
-assert numdigits(99) == 2
-assert numdigits(100) == 3
-assert numdigits(101) == 3
-assert numdigits(999) == 3
-assert numdigits(1000) == 4
-assert numdigits(1001) == 4
-assert numdigits(9999) == 4
-assert numdigits(10000) == 5
-assert numdigits(47000) == 5
-assert numdigits(10001) == 5
-assert numdigits(99999) == 5
-assert numdigits(100000) == 6
-assert numdigits(100001) == 6
-
-def pi(digits):
-  """Return a string of at most ``digits'' characters, prefix of pi."""
-  if digits < 3:
-    return '3'
+def pi(digits0):
+  m0 = maxerr(digits0)
+  lm0 = len(str(m0))
+  digits = lm0 + digits0
+  m = maxerr(digits)
+  lm = len(str(m))
+  if lm > lm0:
+    digits += 1
+    assert lm == lm0 + 1
   u = 10 ** digits
-  y = 4 * (4 * acot(5, u) - acot(239, u))
-  y //= 10 ** (numdigits(maxerr(digits)) - 1)
-  while y % 10 == 0:
-    y //= 10
-  return y // 10
+  return (4 * (4 * arcctg(5, u) - arcctg(239, u))) // (10 ** lm)
 
-assert pi(3) == 31
-assert pi(35) == 3141592653589793238462643383279
-assert pi(36) == 314159265358979323846264338327950
-assert pi(37) == 3141592653589793238462643383279502
-assert pi(77) == 31415926535897932384626433832795028841971693993751058209749445923078164062
+def pigood(digits):
+  if digits <= 1:
+    return '3'
+  ys = str(pi(digits))
+  c = 1
+  while ys[-c] == '0':
+    c += 1
+  return '3.' + ys[1 : -c]
 
-def suffixof(a, b):
-  """Assuming b is a (longer) prefix of a (> 0), then returns the rest of a."""
-  return a % (10 ** (numdigits(a) - numdigits(b)))
-
-assert suffixof(12345, 12) == 345
-assert suffixof(12045, 12) == 45
-assert suffixof(56, 5) == 6
-assert suffixof(567, 5) == 67
+assert pigood(77) == ('3.141592653589793238462643383279502884197169399' +
+                      '375105820974944592307816406286')
 
 if __name__ == '__main__':
-  sys.stdout.write('3.')
-  b = 3
   digits = 8
+  xs = pigood(digits)
+  sys.stdout.write(xs)
+  sys.stdout.flush()
   while True:
-    a = pi(digits)
-    sys.stdout.write(str(suffixof(a, b)))
+    digits <<= 1
+    ys = pigood(digits)
+    assert ys.startswith(xs)
+    sys.stdout.write(ys[len(xs):])
     sys.stdout.flush()
-    b = a
-    digits *= 2
+    xs = ys
